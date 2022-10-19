9c50c4d32cd7693fb643f39ebfd8420b6179c4b2
Alex Allen <99975765+AlexAllen247@users.noreply.github.com>
2022-10-19 15:41:34 +0200

    Completed exercises 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,11 @@
-const repeatString = function() {
+const repeatString = function (string, times) {
+    if (times < 0) {
+        return "ERROR"
+    } else if (times > 0) {
+        return string.repeat(times)
+    } else {
+        return ""
+    }
 
 };
 
