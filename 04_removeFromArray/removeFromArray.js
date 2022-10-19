01dc24c34a14b17148605bf8da312605d82f2a95
Alex Allen <99975765+AlexAllen247@users.noreply.github.com>
2022-10-19 15:58:53 +0200

    Completed exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    const array = args[0]
+    return array.filter(function(value) {
+        return !args.includes(value)
+    })
 };
 
 // Do not edit below this line
