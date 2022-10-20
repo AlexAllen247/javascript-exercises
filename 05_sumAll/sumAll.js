082ba8e3f67e593d837b49331e6af333776ced1e
Alex Allen <99975765+AlexAllen247@users.noreply.github.com>
2022-10-20 07:51:44 +0200

    Completed exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,8 @@
-const sumAll = function() {
-
+const sumAll = function (min, max) {
+    if (isNaN(min) || isNaN(max)) return "ERROR"
+    if (typeof min === "string" || typeof max === "string") return "ERROR"
+    if (min < 0 || max < 0) return "ERROR"
+    return (Math.abs(max - min) + 1) * (min + max) / 2
 };
 
 // Do not edit below this line
