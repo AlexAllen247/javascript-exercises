ce170a226da9c6399b90c6e2460f476fc747f8ec
Alex Allen <99975765+AlexAllen247@users.noreply.github.com>
2022-10-21 06:46:53 +0200

    exercise 09 complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let re = /[\W_]/g
+    let lowerString = string.toLowerCase().replace(re, "")
+    let reverseString = lowerString.split("").reverse().join("")
+    return reverseString === lowerString
 };
 
 // Do not edit below this line
